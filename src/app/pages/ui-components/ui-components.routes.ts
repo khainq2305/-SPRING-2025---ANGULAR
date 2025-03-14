00a0189672baa1cbfebb2120b04bd0e87ff64e50
nguyenquockhai <khainqpc08388@gmail.com>
2025-03-14 17:51:47 +0700

    project setup

---
+++ src/app/pages/ui-components/ui-components.routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+
+// ui
+import { AppChipsComponent } from './chips/chips.component';
+import { AppListsComponent } from './lists/lists.component';
+import { AppFormsComponent } from './forms/forms.component';
+import { AppTablesComponent } from './tables/tables.component';
+
+export const UiComponentsRoutes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'chips',
+        component: AppChipsComponent,
+      },
+      {
+        path: 'lists',
+        component: AppListsComponent,
+      },
+      {
+        path: 'forms',
+        component: AppFormsComponent,
+      },
+      {
+        path: 'tables',
+        component: AppTablesComponent,
+      },
+    ],
+  },
+];
